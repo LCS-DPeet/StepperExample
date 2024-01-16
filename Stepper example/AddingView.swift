ba69d97e50dedecf4a629c2544418e3328fb32bf
Danika Peet <DPeet@lcs.on.ca>
2024-01-16 12:22:00 -0500

    Heres my bracket veiw completed verson of the squares

---
+++ Stepper example/AddingView.swift
@@ -0,0 +1,18 @@
+//
+//  AddingView.swift
+//  Stepper example
+//
+//  Created by Danika Peet on 2024-01-16.
+//
+
+import SwiftUI
+
+struct AddingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingView()
+}
---
+++ Stepper example/BracketView.swift
@@ -0,0 +1,18 @@
+//
+//  BracketView.swift
+//  Stepper example
+//
+//  Created by Danika Peet on 2024-01-16.
+//
+
+import SwiftUI
+
+struct BracketView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BracketView()
+}
